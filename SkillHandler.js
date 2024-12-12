fe79ecb34b3b9af9bdf189c7c6a94f1a852e3e2d
saesarandom <saesarandom@gmail.com>
2024-12-11 19:31:19 -0600

    inner sight dalsi

--- SkillHandler.js
+++ SkillHandler.js
@@ -225,6 +225,24 @@ class SkillHandler {
             },
             prerequisites: ["jab", "powerStrike", "poisonJavelin", "lightningBolt", "plagueJavelin"],
             synergies: ["powerstrike,3", "lightningbolt,3"]
+            },
+          
+          innerSight: {
+            name: "Inner Sight",
+            description: "Illuminates nearby enemies",
+            maxLevel: 99,
+            levelData: {
+              enemyAr: {
+                base: [10,	11,	12,	13,	14,	15,	16,	17,	18,	19,	20,	21,	22,	23,	24,	25,	26,	27,	28,	29,	30,	31,	32,	33,	34,	35,	36,	37,	38,	39,	40,	40,	40]
+              },
+              manaCost: {
+                base: 5,
+                perLevel: 0,
+                formula: "base + (perLevel * (level - 1))"
+              }
+            },
+            prerequisites: [],
+            synergies: []
             }
 
     };
