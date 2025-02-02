b0feae000d8f1b3ad4b1ff11116d57f62668e08d
saesarandom <saesarandom@gmail.com>
2025-02-01 19:03:26 -0600

    added more set armors

--- items.js
+++ items.js
@@ -1473,6 +1473,158 @@ const itemList = {
       req: -15,
     },
   },
+
+  "Arctic Furs": {
+    description:
+      "Arctic Furs<br>Quilted Armor<br>Base Maximum Sockets: 2 (3 for upgraded elite versions)<br>Defense: 51<br>Defense (2 Items): 48-348<br>Required Strength: 12<br>Required Level: 2<br>+325% Enhanced Defense<br>All Resistances +10<br>+[3-297] Defense (+3 per Character Level) (2 Items)<br>Cold Resist +15% (3 Items)<br>",
+    properties: {
+      defense: 1246,
+      reqstr: 145,
+      reqlvl: 55,
+      firedmgmin: 160,
+      firedmgmax: 340,
+      edef: 180,
+      str: 8,
+      vit: 10,
+      coldres: 35,
+      corpseexplosionsk: 40,
+      reanimate: 5, //Returned
+      req: -15,
+    },
+  },
+
+  "Berserker's Hauberk": {
+    description:
+      "Berserker's Hauberk<br>Splint Mail<br>Base Maximum Sockets: 2 (3 for upgraded versions)<br>Defense: 84<br>Defense (2 Items): 85-529<br>Required Strength: 51<br>Required Level: 3<br>+1 to Barbarian Skills<br>Magic Damage Taken Reduced by 2<br>+[4-445] Defense (+4.5 per Character Level) (2 Items)<br>+[3-297]% Enhanced Maximum Damage (+3% per Character Level) (Complete Set)<br>",
+    properties: {
+      defense: 1246,
+      reqstr: 145,
+      reqlvl: 55,
+      firedmgmin: 160,
+      firedmgmax: 340,
+      edef: 180,
+      str: 8,
+      vit: 10,
+      coldres: 35,
+      corpseexplosionsk: 40,
+      reanimate: 5, //Returned
+      req: -15,
+    },
+  },
+
+  "Sigon's Shelter": {
+    description:
+      "Sigon's Shelter<br>Gothic Plate<br>Defense: 161<br>Required Strength: 70<br>Required Level: 6<br>+25% Enhanced Defense<br>Lightning Resist +30%<br>Attacker Takes Damage of 20 (2 Items)<br>",
+    properties: {
+      defense: 1246,
+      reqstr: 145,
+      reqlvl: 55,
+      firedmgmin: 160,
+      firedmgmax: 340,
+      edef: 180,
+      str: 8,
+      vit: 10,
+      coldres: 35,
+      corpseexplosionsk: 40,
+      reanimate: 5, //Returned
+      req: -15,
+    },
+  },
+
+  "Isenhart's Case": {
+    description:
+      "Isenhart's Case<br>Breast Plate<br>Defense: 98<br>Defense (2 Items): 98-296 (was 107-306)<br>Required Strength: 30<br>Required Level: 8<br>+40 Defense<br>Magic Damage Taken Reduced by 4<br>+[2-198] Defense (+2 per Character Level) (2 Items)<br>Cold Resist +[1-99]% (+1% per Character Level) (3 Items)<br>",
+    properties: {
+      defense: 1246,
+      reqstr: 145,
+      reqlvl: 55,
+      firedmgmin: 160,
+      firedmgmax: 340,
+      edef: 180,
+      str: 8,
+      vit: 10,
+      coldres: 35,
+      corpseexplosionsk: 40,
+      reanimate: 5, //Returned
+      req: -15,
+    },
+  },
+
+  "Cathan's Mesh": {
+    description:
+      "Cathan's Mesh<br>Chain Mail<br>Base Maximum Sockets: 2 (3 for upgraded versions)<br>Defense: 86<br>Required Strength: 24<br>Required Level: 11<br>+20% Faster Run/Walk<br>+15 Defense<br>Requirements -50%<br>Attacker Takes Damage of [1-99] (1 per Character Level) (2 Items)<br>Fire Resist +30% (3 Items)<br>",
+    properties: {
+      defense: 1246,
+      reqstr: 145,
+      reqlvl: 55,
+      firedmgmin: 160,
+      firedmgmax: 340,
+      edef: 180,
+      str: 8,
+      vit: 10,
+      coldres: 35,
+      corpseexplosionsk: 40,
+      reanimate: 5, //Returned
+      req: -15,
+    },
+  },
+
+  "Angelic Mantle": {
+    description:
+      "Angelic Mantle<br>Ring Mail<br>Defense: 72<br>Defense (2 Items): 222<br>Required Strength: 36<br>Required Level: 12<br>+[50-80]% Damage to Undead<br>+40% Enhanced Defense<br>Physical Damage Taken Reduced by 3<br>+150 Defense (2 Items)<br>Fire Resist +50% (3 Items)<br>",
+    properties: {
+      defense: 1246,
+      reqstr: 145,
+      reqlvl: 55,
+      firedmgmin: 160,
+      firedmgmax: 340,
+      edef: 180,
+      str: 8,
+      vit: 10,
+      coldres: 35,
+      corpseexplosionsk: 40,
+      reanimate: 5, //Returned
+      req: -15,
+    },
+  },
+
+  "Vidala's Ambush": {
+    description:
+      "Vidala's Ambush<br>Leather Armor<br>Base Maximum Sockets: 2 (3 for upgraded elite versions)<br>Defense: 67<br>Defense (3 Items): 67-314<br>Required Strength: 15<br>Required Level: 14<br>+50 Defense<br>+[15-25] to Dexterity<br>Fire Resist +24% (2 Items)<br>+[2-247] Defense (+2.5 per Character Level) (3 Items)<br>",
+    properties: {
+      defense: 1246,
+      reqstr: 145,
+      reqlvl: 55,
+      firedmgmin: 160,
+      firedmgmax: 340,
+      edef: 180,
+      str: 8,
+      vit: 10,
+      coldres: 35,
+      corpseexplosionsk: 40,
+      reanimate: 5, //Returned
+      req: -15,
+    },
+  },
+
+  "Arcanna's Flesh": {
+    description:
+      "Arcanna's Flesh<br>Light Plate<br>Defense: 99<br>Defense (2 Items): 199<br>Required Strength: 41<br>Required Level: 15<br>+10% Faster Cast Rate<br>Physical Damage Taken Reduced by 6<br>+2 to Light Radius<br>+100 Defense (2 Items)<br>+20 to Energy (3 Items)<br>Regenerate Mana 20% (Complete Set)<br>",
+    properties: {
+      defense: 1246,
+      reqstr: 145,
+      reqlvl: 55,
+      firedmgmin: 160,
+      firedmgmax: 340,
+      edef: 180,
+      str: 8,
+      vit: 10,
+      coldres: 35,
+      corpseexplosionsk: 40,
+      reanimate: 5, //Returned
+      req: -15,
+    },
+  },
   // UNIQUE WEAPONS AXES //
 
   "The Gnasher": {
