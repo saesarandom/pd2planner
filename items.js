e49eb7673153933c73f9fb242653bee4736cb507
saesa <46235782+saesarandom@users.noreply.github.com>
2025-11-08 19:40:57 +0100

    Update items.js

--- items.js
+++ items.js
@@ -3175,8 +3175,6 @@ const itemList = {
   },
 
   "True Silver": {
-    description:
-      "True Silver<br>Maiden Javelin<br>Base Speed Modifier: -10<br>One-Hand Damage: 17 to 252, Avg 141.5<br>Throw Damage: 13 to 270, Avg 134.5<br>Required Strength: 33<br>Required Dexterity: 47<br>Required Level: 20<br>+40% Increased Attack Speed<br>+120% Enhanced Damage<br>+[2-222] to Maximum Damage (+2.25 per Character Level)<br>15% Chance of Crushing Blow<br>45% Better Chance of Getting Magic Items<br>",
     properties: {
       javelin: 1,
       speed: -10,
@@ -3188,10 +3186,10 @@ const itemList = {
       reqdex: 47,
       reqlvl: 20,
       ias: 40,
-      edmg: 120,
+      edmg: { min: 80, max: 120, current: 120 },
       maxdmgperlvl: 2.25, //asi tu nevim jestli to jde
-      cb: 15,
-      magicfind: 45,
+      cb: { min: 10, max: 15, current: 15 },
+      magicfind: { min: 25, max: 45, current: 45 },
     },
   },
 
